84be76ad0209e4558fa059027f5fdbb32ef98385
Amr <AmrAhmed24555@gmail.com>
2026-09-07 23:36:49 +0300

    66. Plus One

--- ToBeContinued/ToBeContinued.cpp
+++ ToBeContinued/ToBeContinued.cpp
@@ -292,7 +292,31 @@ int lengthOfLastWord(string s) {
 
 
 
+vector<int> plusOne(vector<int>& digits) {
+    
+    int carry = 1;
 
+    for (int i = digits.size()-1; i >= 0; i--) {
+
+        if (!carry)
+            break;
+
+        if (digits[i] == 9) {
+            digits[i] = 0;
+        }
+        else {
+            digits[i] += carry;
+            carry = 0;
+        }
+
+    }
+
+    if (carry) {
+        digits.insert(digits.begin(), 1);
+    }
+
+    return digits;
+}
 
 
 
